37ef72b1178faa34ff5bdda44da0f8d57c48e99f
mbprtpnix <78172971+mbprtpnix@users.noreply.github.com>
2021-02-22 17:45:56 +0200

    Add files via upload

---
+++ startup.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+pacman \
+    -Syyu \
+    --needed \
+    --noconfirm \
+    reflector \
+    bash-completion \
+    python \
+    neovim \
+    vim \
+    git \
+    curl \
+    wget \
+    rsync \
+    rclone \
+    stow \
+    htop \
+    devtools \
+    archiso \
+    pacman-contrib
+
+reflector \
+    --latest 5 \
+    --age 6 \
+    --protocol https \
+    --sort rate \
+    --save /etc/pacman.d/mirrorlist
